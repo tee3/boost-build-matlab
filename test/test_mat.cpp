e2b288f1c5d2913e3e46150d8a6594563175c4de
Thomas Brown <tabsoftwareconsulting@gmail.com>
2017-10-06 14:55:47 -0400

    Provide the file for testing .mat files on the command line.

--- test/test_mat.cpp
+++ test/test_mat.cpp
@@ -1,11 +1,22 @@
+#include <iostream>
+
 #include "mat.h"
 
 int
-main ()
+main (int argc, char * argv [])
 {
-   MATFile * mp = matOpen ("test.mat","r");
+   if (argc != 2)
+   {
+      std::cout << "usage: test_mat <path-to-mat>\n";
+
+      return 1;
+   }
+
+   MATFile * mp = matOpen (argv [1],"r");
    if (!mp)
    {
+      std::cout << "error: could not open " << argv [1] << "\n";
+
       return 1;
    }
 
